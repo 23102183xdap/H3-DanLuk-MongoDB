9babb005a3d151acfd245576f6bdfdd451ab4c08
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2021-09-13 11:27:07 +0200

    customer added

---
+++ app/models/customer.model.js
@@ -0,0 +1,30 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        firstname: String,
+        lastname: String,
+        birthdate: Date(),
+        
+        // TODO loaned default null
+        // TODO loaned null after 30 days passed
+        currentlyLoaned: {
+            book: {
+                type: mongoose.Schema.Types.ObjectID,
+                ref: "book"
+            },
+            loanDate: Date()
+        }
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const Customer = mongoose.model("customer", schema);
+    return Customer;
+  };
+  
